127d1039fc659bd2ea9803b1dcd39ff865e8d327
Egor Kolesnikov <freethinkel@gmail.com>
2024-04-25 13:56:55 +0500

    try to build release

---
+++ scripts/pack_xterm.js
@@ -0,0 +1,21 @@
+import { execSync } from "child_process";
+const exec = (line) => console.log(execSync(line) + "");
+
+exec(
+  [
+    "cd ../xterm.js/",
+    "yarn package",
+    "yarn pack",
+    "cp ./xterm-*.tgz ../fluffy/patched/xterm",
+  ].join(" && ")
+);
+exec(
+  [
+    "cd ../xterm.js/",
+    "yarn package",
+    "cd ./addons/addon-webgl",
+    "yarn package",
+    "yarn pack",
+    "cp -R ./xterm-*.tgz ../../../fluffy/patched/xterm-addon-webgl",
+  ].join(" && ")
+);
